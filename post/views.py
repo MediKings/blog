63e1d9b044a10d19d91dd5d77c827768e0f55a7b
Medi King's <jehovahmbote3@gmail.com>
2023-01-27 19:18:13 +0100

    Contact form

--- post/views.py
+++ post/views.py
@@ -5,3 +5,11 @@ def Home(request):
     template_name = 'post/index.html'
     context = {}
     return render(request, template_name, context)
+
+
+def Contact(request):
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    template_name = 'post/contact.html'
+    context = {}
+    return render(request, template_name, context)
